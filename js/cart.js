e8e844a3ade0e629bc68a02481f6d44af8bbd139
GabrielGonzalez <gabriel-97-312@hotmail.com>
2022-10-25 23:53:31 -0300

    optimized

--- js/cart.js
+++ js/cart.js
@@ -2,15 +2,15 @@ let cartInfo = {};
 const exchangeURL = 'https://cotizaciones-brou.herokuapp.com/api/currency/latest'
 let exchange = [];
 
-document.addEventListener("DOMContentLoaded", async () =>{
-    
-    if((localStorage.getItem('clientCart') == '[]') || (localStorage.getItem('clientCart') == undefined) ){
-        const response = await fetch(CART_INFO_URL+"25801.json")
+document.addEventListener("DOMContentLoaded", async () => {
+
+    if ((localStorage.getItem('clientCart') == '[]') || (localStorage.getItem('clientCart') == undefined)) {
+        const response = await fetch(CART_INFO_URL + "25801.json")
         const result = await response.json()
         cartInfo = result.articles;
         localStorage.setItem('clientCart', JSON.stringify(cartInfo));
-        
-    }else{
+
+    } else {
         cartInfo = JSON.parse(localStorage.getItem('clientCart'))
     }
 
@@ -20,74 +20,81 @@ document.addEventListener("DOMContentLoaded", async () =>{
 
     showCart(cartInfo)
     calcSubT();
-    document.querySelectorAll('#creditcard', '#banktransfer').forEach(input =>{
+    document.querySelectorAll('#creditcard', '#banktransfer').forEach(input => {
         input.checked = false
-    })
+    });
 
 })
 
-document.addEventListener('input',(e) => {
-    if(e.target.type == 'number'){
+document.addEventListener('input', (e) => {
+    if (e.target.type == 'number') {
         const itemTarget = e.target.parentNode.nextElementSibling.lastChild;
         prodLine = cartInfo.find((prod) => prod.id == e.target.id);
-        
+
         e.data == null ?
-        itemTarget.innerHTML = `
-            <span class='costToAdd${prodLine.currency}'>${prodLine.unitCost}</span>`:
-        itemTarget.innerHTML = `
+            itemTarget.innerHTML = `
+            <span class='costToAdd${prodLine.currency}'>${prodLine.unitCost}</span>` :
+            itemTarget.innerHTML = `
             <span class='costToAdd${prodLine.currency}'>${prodLine.unitCost * parseInt(e.target.value)}</span>`;
 
         calcSubT()
     }
-    
+
 })
 
 buy.addEventListener('click', buyValidation)
 
-function calcSubT(){
+function calcSubT() {
     subtotal = 0
     document.querySelectorAll(".costToAddUSD").forEach(cost => {
         subtotal += parseInt(cost.innerHTML)
     })
     document.querySelectorAll(".costToAddUYU").forEach(cost => {
-        subtotal += parseInt(cost.innerHTML)/exchange.rates.USD.sell
+        subtotal += parseInt(cost.innerHTML) / exchange.rates.USD.sell
     })
-    subT.innerHTML = `USD ${subtotal.toFixed(2)}`
+    subT.innerHTML = `USD ${(Math.round(subtotal * 100)/100).toLocaleString('en-US', {minimumFractionDigits: 2})}`
 
     shipPercent = parseFloat(document.querySelector('input[name=shipCost]:checked').value)
     shipCost = subtotal * shipPercent
-    shipP.innerHTML = `USD ${shipCost.toFixed(2)}`
+    shipP.innerHTML = `USD ${(Math.round(shipCost * 100)/100).toLocaleString('en-US', {minimumFractionDigits: 2})}`
 
-    total.innerHTML = `USD ${(subtotal + shipCost).toFixed(2)}`
+    totalPrice = subtotal + shipCost
+    total.innerHTML = `USD ${(Math.round(totalPrice * 100)/100).toLocaleString('en-US', {minimumFractionDigits: 2})}`
 
 }
 
-function showCart(array){
+function showCart(array) {
     tableCart.innerHTML = ""
-    for(prod of array){
-        const {image, name, currency, unitCost, id} = prod
+    for (prod of array) {
+        const { image, name, currency, unitCost, id } = prod
 
         tableCart.innerHTML += `
         <tr class="align-middle">
-            <th scope="row"><img src="${image}" class="img-thumbnail" alt="..." style="width: 100px;"></th>
-            <td>${name}</td>
-            <td>${currency} ${unitCost}</td>
-            <td><input id="${id}" class="form-control" type="number" style="width: 4rem" min='0' value = "1" inputmode="numeric"></td>
+            <th scope="row"><img src="${image}" class="img-responsive" alt="..." style="width: 100px;"></th>
+            <td>
+                ${name}
+            </td>
+            <td>
+                ${currency} ${unitCost}
+            </td>
+            <td>
+                <input id="${id}" class="form-control" type="number" style="width: 4rem" min='0' value = "1" inputmode="numeric">
+            </td>
             <td style="width: 150px"><b>${currency}</b> <b><span class="costToAdd${currency}" >${unitCost}</b></span></td>
             <td class='text-center' style='width:70px'>
-            <button onclick="deleteCartItem(${id})" type="button" class="btn btn-outline-danger">
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"></path>
-                </svg>
-            </button>
+                <button onclick="deleteCartItem(${id})" type="button" class="btn btn-outline-danger">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"></path>
+                    </svg>
+                </button>
             </td>
         </tr>
         `
     }
 }
 
-function paymentMethod(){
-    if(document.getElementById('creditcard').checked){
+function paymentMethod() {
+    if (document.getElementById('creditcard').checked) {
         document.querySelectorAll('.banktransfer').forEach(input => {
             input.disabled = true
         })
@@ -95,7 +102,7 @@ function paymentMethod(){
             input.disabled = false
         })
         payMethod.innerHTML = 'Tarjeta de credito'
-    }else{
+    } else {
         document.querySelectorAll('.banktransfer').forEach(input => {
             input.disabled = false
         })
@@ -106,42 +113,83 @@ function paymentMethod(){
     }
 }
 
-function buyValidation(){
-    errorCounter = 0
+
+function quantityValidation() {
+    error = 0
     document.querySelectorAll('input[type=number]').forEach(input => {
-        if((input.value == "") || (parseInt(input.value) <= 0)){
-            errorCounter += 1
-            input.classList.add('border','border-danger')
-        }else{
-            input.classList.remove('border','border-danger')
-        }        
+        if ((input.value == "") || (parseInt(input.value) <= 0)) {
+            error += 1
+            input.classList.add('border', 'border-danger')
+        } else {
+            input.classList.remove('border', 'border-danger')
+        }
     })
 
-    document.querySelectorAll('input[name=address]').forEach(input =>{
-        if(input.value == ""){
-            errorCounter += 1
+    return (error == 0 ? (itemTable.classList.remove('is-invalid'), true) :
+        (itemTable.classList.add('is-invalid'), false))
+}
+
+function addressValidation() {
+    error = 0
+    document.querySelectorAll('input[name=address]').forEach(input => {
+        if (input.value.trim() == "") {
+            error += 1
             input.classList.add('is-invalid')
-        }else{
+        } else {
             input.classList.remove('is-invalid')
         }
     })
 
-    payError = 0
-    document.querySelectorAll('input.creditcard, input.banktransfer').forEach(input =>{ 
-        if((!input.disabled) && (input.value == '')){
+    return (error == 0)
+}
+
+function shipValidation() {
+    const shipButtons = document.getElementsByName('shipCost')
+    for (let i = 0; i < shipButtons.length; i++) {
+        if (shipButtons[i].checked) {
+            shipPreferences.classList.remove('is-invalid')
+            return true
+        } else {
+            shipPreferences.classList.add('is-invalid')
+        }
+    }
+    return false
+}
+
+function paymentValidation() {
+    error = 0
+    document.querySelectorAll('input.creditcard, input.banktransfer').forEach(input => {
+        if ((!input.disabled) && (input.value == '')) {
             methodCont.classList.add('is-invalid')
-            payError += 1
-            errorCounter += 1
+            error += 1
         }
 
     })
-    if(payError == 0){
-        methodCont.classList.remove('is-invalid')
+
+    //Estoy aprendiendo jQuery y lo utilice para probar
+    return (error == 0 ? ($('#methodCont').addClass('is-invalid'), true) :
+        false)
+
+
+}
+
+function buyValidation() {
+    //Caso que se hayan eliminado todos los articulos del carrito y se clickee el boton de finalizar compra
+    if (document.getElementById('tableCart').childElementCount == 0) {
+        buyAlert.innerHTML = `
+        <div class="alert alert-danger alert-dismissible" role="alert" style='z-index: 100000'>
+            <div>Debe tener al menos un articulo en el carrito para poder comprar!</div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        `
+        return
     }
 
-    
-
-    if(errorCounter == 0){
+    quantity = quantityValidation()
+    address = addressValidation()
+    ship = shipValidation()
+    payment = paymentValidation()
+    if ((quantity) && (address) && (ship) && (payment)) {
         buyAlert.innerHTML = `
         <div class="alert alert-success alert-dismissible" role="alert" style='z-index: 100000'>
             <div>Has comprado con éxito!</div>
@@ -153,11 +201,11 @@ function buyValidation(){
 }
 
 //Desafiate entrega 6
-function deleteCartItem(id){
+function deleteCartItem(id) {
     actualCart = JSON.parse(localStorage.getItem('clientCart'));
-    afterDelete = actualCart.filter( prod => prod.id != id);
+    afterDelete = actualCart.filter(prod => prod.id != id);
     localStorage.setItem('clientCart', JSON.stringify(afterDelete));
 
-    showCart(afterDelete); 
+    showCart(afterDelete);
     calcSubT();
 }
--- js/product-info.js
+++ js/product-info.js
@@ -8,18 +8,19 @@ const toastLiveExample = document.getElementById('liveToast');
 document.addEventListener('DOMContentLoaded', () => {
     let result = {}
     getJSONData(URL)
-    .then((response) =>{
-        if(response.status == 'ok'){
-            result = response
-            return result
-            
-        }})
-    .then((result) => {
-            const {data: itemArray} = result;
-            const { id, category, cost, 
-                    currency, description, 
-                    images, name, soldCount, relatedProducts } = itemArray
-            toCartS = {id: id, name: name, unitCost:cost, currency, image: images[0]};            
+        .then((response) => {
+            if (response.status == 'ok') {
+                result = response
+                return result
+
+            }
+        })
+        .then((result) => {
+            const { data: itemArray } = result;
+            const { id, category, cost,
+                currency, description,
+                images, name, soldCount, relatedProducts } = itemArray
+            toCartS = { id: id, name: name, unitCost: cost, currency, image: images[0] };
             document.getElementById('itemName').innerHTML = name
             document.getElementById('item-cont').innerHTML = `
             <h5><strong>Precio</strong></h5>
@@ -31,37 +32,37 @@ document.addEventListener('DOMContentLoaded', () => {
             <h5><strong>Cantidad de vendidos</strong></h5>
             <p>${soldCount} vendidos</p>
             `
-            for(let i = 0; i < images.length ; i++){
-                if(i == 0){
-                document.getElementById('img-container').innerHTML += `
-                <div class="carousel-item active" style='height: 400px'>
-                  <img src="${images[i]}" class="d-block w-100" alt="..." style="max-height:100%">
-                </div>
-                `
-                document.getElementById('indicators').innerHTML += `
-                <button style="width: 25%" type="button" data-bs-target="#productCarousel" data-bs-slide-to="${i}" class="active" aria-current="true" aria-label="Slide ${i+1}">
-                    <img src="${images[i]}" style="height: auto; object-fit: cover" class="d-block w-100" alt="...">
-                </button>
-                `
-                } else{
-                document.getElementById('img-container').innerHTML += `
-                <div class="carousel-item" style='height: 400px'>
-                  <img src="${images[i]}" class="d-block w-100" style="max-height:100%;"  alt="...">
-                </div>
-                `
-                document.getElementById('indicators').innerHTML += `
-                <button style="width: 25%" type="button" data-bs-target="#productCarousel" data-bs-slide-to="${i}" aria-label="Slide ${i+1}">
-                    <img src="${images[i]}" style="height: auto; object-fit: cover" class="d-block w-100" alt="...">
-                </button>
-                `
-                }
+            for (let i = 0; i < images.length; i++) {
+                if (i == 0) {
+                    document.getElementById('img-container').innerHTML += `
+                    <div class="carousel-item active" style='height: 400px'>
+                        <img src="${images[i]}" class="d-block w-100" alt="..." style="max-height:100%">
+                    </div>
+                    `
+                    document.getElementById('indicators').innerHTML += `
+                    <button style="width: 25%" type="button" data-bs-target="#productCarousel" data-bs-slide-to="${i}" class="active" aria-current="true" aria-label="Slide ${i + 1}">
+                        <img src="${images[i]}" style="height: auto; object-fit: cover" class="d-block w-100" alt="...">
+                    </button>
+                    `
+                } else {
+                    document.getElementById('img-container').innerHTML += `
+                    <div class="carousel-item" style='height: 400px'>
+                        <img src="${images[i]}" class="d-block w-100" style="max-height:100%;"  alt="...">
+                    </div>
+                    `
+                    document.getElementById('indicators').innerHTML += `
+                    <button style="width: 25%" type="button" data-bs-target="#productCarousel" data-bs-slide-to="${i}" aria-label="Slide ${i + 1}">
+                        <img src="${images[i]}" style="height: auto; object-fit: cover" class="d-block w-100" alt="...">
+                    </button>
+                    `
+                };
 
-                
-                
+
+
             }
 
-            for(let relProd of relatedProducts){
-                const {image, name, id} = relProd;
+            for (let relProd of relatedProducts) {
+                const { image, name, id } = relProd;
                 relatedCont.innerHTML += `
                 <div onclick="setProdID(${id})" class="card m-3 list-group-item list-group-item-action cursor-active" style="width: 25%; border: solid lightgrey 1px; padding: 0">
                     <img src="${image}" class="card-img-top" alt="...">
@@ -73,73 +74,75 @@ document.addEventListener('DOMContentLoaded', () => {
                 </div>
                 `
             }
-            
+
         })
-    
+
     getJSONData(commURL)
         .then((response) => {
-            if(response.status == 'ok'){
+            if (response.status == 'ok') {
                 result = response
                 return result;
-                }})
+            }
+        })
         .then((result) => {
-            const {data: commArray} = result;
-            for(comment of commArray){
-                const {dateTime, description,user} = comment
+            const { data: commArray } = result;
+            for (comment of commArray) {
+                const { dateTime, description, user } = comment
                 showComment(user, dateTime, description);
             }
-            
+
             document.querySelectorAll('.stars').forEach((comment, index) => {
-                for(let i = 0; i < commArray[index].score; i++){
+                for (let i = 0; i < commArray[index].score; i++) {
                     comment.children[i].classList.add('checked')
                 }
             })
-            if(localStorage.getItem(`${prodID}`) != null){
+
+            if (localStorage.getItem(`${prodID}`) != null) {
                 document.getElementById('comment-list').innerHTML += localStorage.getItem(`${prodID}`)
             }
-            
-             
-        })  
+
+
+        })
 })
 
 //Desafiate entrega 5
-function addCart(prodToCart){
+function addCart(prodToCart) {
     let actual = []
-    if(localStorage.getItem('clientCart') == null){
+    if (localStorage.getItem('clientCart') == null) {
         actual.push(prodToCart);
         localStorage.setItem('clientCart', JSON.stringify(actual));
         toastShow();
-    }else{
-        JSON.parse(localStorage.getItem('clientCart')).forEach(pr => { 
+    } else {
+        JSON.parse(localStorage.getItem('clientCart')).forEach(pr => {
             actual.push(pr)
         })
-        if(actual.find(element => element.id == prodToCart.id) == undefined){
+        if (actual.find(element => element.id == prodToCart.id) == undefined) {
             actual.push(prodToCart);
             localStorage.setItem('clientCart', JSON.stringify(actual));
             toastShow()
-        }else{
+        } else {
             alreadyInCart();
         }
     }
 }
 
-function alreadyInCart(){
+function alreadyInCart() {
     document.getElementById('liveToast').classList.replace('bg-success', 'bg-danger');
     document.getElementById('toastBody').innerHTML = 'El producto ya existe en el carrito!'
     const toast = new bootstrap.Toast(toastLiveExample)
     toast.show()
 }
-function toastShow(){
+function toastShow() {
     const toast = new bootstrap.Toast(toastLiveExample)
     toast.show()
 }
 
-function setProdID(id){
+function setProdID(id) {
     localStorage.setItem('prodID', id)
     window.location.href = 'product-info.html'
 }
 
-function showComment(user, dateTime, description,){
+function showComment(user, dateTime, description,) {
     document.getElementById('comment-list').innerHTML += `
     <li class="list-group-item d-flex justify-content-between align-items-start comments">
         <div class="ms-2 me-auto">
@@ -161,13 +164,13 @@ function showComment(user, dateTime, description,){
 
 //Desafiate
 document.getElementById("opinion").addEventListener('click', () => {
-    if(checkUserComment()){
+    if (checkUserComment()) {
         return
     };
     let date = new Date();
-	let dateFormat = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
-	let timeFormat = date.getHours()+":"+date.getMinutes()+":"+ date.getSeconds();
-	let dateTime = dateFormat+" "+timeFormat;	
+    let dateFormat = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+    let timeFormat = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    let dateTime = dateFormat + " " + timeFormat;
     const description = document.getElementById('description');
     const puntuation = document.getElementById('punt');
     document.getElementById('comment-list').innerHTML += `
@@ -188,32 +191,33 @@ document.getElementById("opinion").addEventListener('click', () => {
         </li>
     `;
 
-    for(let i = 0; i < puntuation.value; i ++){
-        document.querySelectorAll('.stars')[document.querySelectorAll('.stars').length -1]
-        .children[i].classList.add('checked')
+    for (let i = 0; i < puntuation.value; i++) {
+        document.querySelectorAll('.stars')[document.querySelectorAll('.stars').length - 1]
+            .children[i].classList.add('checked')
     };
     description.value = '';
     puntuation.value = 1;
     toLocalS = ""
-    for(let userComm of document.querySelectorAll('.usercomments')){
+    for (let userComm of document.querySelectorAll('.usercomments')) {
         toLocalS += `
         <li class="list-group-item d-flex justify-content-between align-items-start usercomments">
             ${userComm.innerHTML}
         </li>
         `
-        
+
     }
-    localStorage.setItem(`${prodID}`, toLocalS )
+    localStorage.setItem(`${prodID}`, toLocalS)
 })
 
 function checkUserComment() {
     let usersComment = document.querySelectorAll('.user');
-    for(let user of usersComment){
-        if(user.innerHTML == loggedUser){
+    for (let user of usersComment) {
+        if (user.innerHTML == loggedUser) {
             alert('Ya ha introducido un comentario en este producto.');
             return true;
         }
-    ;}    
+        ;
+    }
 }
 
 
